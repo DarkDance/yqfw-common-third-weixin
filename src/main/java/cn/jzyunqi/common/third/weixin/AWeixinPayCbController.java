9dcffb986c785056502ec572e043b65760a1b0d3
DarkDance <57419153@qq.com>
2024-03-09 20:22:18 +0800

    增加支付回调

--- src/main/java/cn/jzyunqi/common/third/weixin/AWeixinPayCbController.java
+++ src/main/java/cn/jzyunqi/common/third/weixin/AWeixinPayCbController.java
@@ -2,8 +2,11 @@ package cn.jzyunqi.common.third.weixin;
 
 import cn.jzyunqi.common.exception.BusinessException;
 import cn.jzyunqi.common.feature.pay.OrderQueryResult;
+import cn.jzyunqi.common.feature.pay.PayCallbackDto;
+import cn.jzyunqi.common.feature.pay.PayCbService;
 import cn.jzyunqi.common.third.weixin.client.WeixinPayV3Client;
 import cn.jzyunqi.common.third.weixin.model.callback.PayResultCb;
+import cn.jzyunqi.common.utils.StringUtilPlus;
 import jakarta.annotation.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,10 +22,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 public abstract class AWeixinPayCbController {
 
-    private static final String NOT_SUPPORT = "Not support now!";
-
     @Resource
     private WeixinPayV3Client weixinPayV3Client;
+
+    @Resource
+    private PayCbService payCbService;
 
     /**
      * 微信支付回调
@@ -32,9 +36,21 @@ public abstract class AWeixinPayCbController {
     @RequestMapping
     @ResponseBody
     public void payApplyWeixinCallback(@RequestBody PayResultCb payResultCb, @RequestBody String body, @RequestHeader HttpHeaders headers) throws BusinessException {
-        OrderQueryResult orderQueryResult = weixinPayV3Client.decryptPayCallback(headers.toSingleValueMap(), body, payResultCb);
-        processWeixinPayCallback(orderQueryResult);
+        if (StringUtilPlus.equalsIgnoreCase(payResultCb.getEventType(), "TRANSACTION.SUCCESS")) {
+            OrderQueryResult orderQueryResult = weixinPayV3Client.decryptPayCallback(headers.toSingleValueMap(), body, payResultCb);
+            if(orderQueryResult != null){
+                PayCallbackDto payCallbackDto = new PayCallbackDto();
+                payCallbackDto.setActualPayType("weixin_pay"); //实际支付方式
+                payCallbackDto.setApplyPayNo(orderQueryResult.getOutTradeNo()); //申请支付单号
+                payCallbackDto.setActualPayNo(orderQueryResult.getTransactionId()); //微信支付单号
+                payCallbackDto.setActualPayAmount(orderQueryResult.getTotalFee()); //支付金额
+                payCallbackDto.setReturnParam(orderQueryResult.getResponseStr());
+                payCbService.paySuccess(payCallbackDto);
+            }else{
+                throw new BusinessException("common_weixin_pay_call_back_failed");
+            }
+        } else {
+            throw new BusinessException("common_weixin_pay_call_back_failed");
+        }
     }
-
-    protected abstract void processWeixinPayCallback(OrderQueryResult orderQueryResult);
 }
