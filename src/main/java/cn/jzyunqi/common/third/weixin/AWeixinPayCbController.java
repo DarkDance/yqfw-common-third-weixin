3dac7147c8c21525c0905e14796356f2c5d07a98
DarkDance <57419153@qq.com>
2024-03-09 20:26:26 +0800

    增加支付回调

--- src/main/java/cn/jzyunqi/common/third/weixin/AWeixinPayCbController.java
+++ src/main/java/cn/jzyunqi/common/third/weixin/AWeixinPayCbController.java
@@ -2,7 +2,6 @@ package cn.jzyunqi.common.third.weixin;
 
 import cn.jzyunqi.common.exception.BusinessException;
 import cn.jzyunqi.common.feature.pay.OrderQueryResult;
-import cn.jzyunqi.common.feature.pay.PayCallbackDto;
 import cn.jzyunqi.common.feature.pay.PayCbService;
 import cn.jzyunqi.common.third.weixin.client.WeixinPayV3Client;
 import cn.jzyunqi.common.third.weixin.model.callback.PayResultCb;
@@ -39,13 +38,7 @@ public abstract class AWeixinPayCbController {
         if (StringUtilPlus.equalsIgnoreCase(payResultCb.getEventType(), "TRANSACTION.SUCCESS")) {
             OrderQueryResult orderQueryResult = weixinPayV3Client.decryptPayCallback(headers.toSingleValueMap(), body, payResultCb);
             if(orderQueryResult != null){
-                PayCallbackDto payCallbackDto = new PayCallbackDto();
-                payCallbackDto.setActualPayType("weixin_pay"); //实际支付方式
-                payCallbackDto.setApplyPayNo(orderQueryResult.getOutTradeNo()); //申请支付单号
-                payCallbackDto.setActualPayNo(orderQueryResult.getTransactionId()); //微信支付单号
-                payCallbackDto.setActualPayAmount(orderQueryResult.getTotalFee()); //支付金额
-                payCallbackDto.setReturnParam(orderQueryResult.getResponseStr());
-                payCbService.paySuccess(payCallbackDto);
+                payCbService.paySuccess("weixin_pay", orderQueryResult.getOutTradeNo(), orderQueryResult.getTransactionId(), orderQueryResult.getTotalFee(), orderQueryResult.getResponseStr());
             }else{
                 throw new BusinessException("common_weixin_pay_call_back_failed");
             }
