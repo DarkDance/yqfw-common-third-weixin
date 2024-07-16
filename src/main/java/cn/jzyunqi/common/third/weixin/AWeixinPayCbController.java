3dea3e6e036af19f06ae1a5abcb9a97d2046d6fd
DarkDance <57419153@qq.com>
2024-07-16 15:47:50 +0800

    支付抽取

--- src/main/java/cn/jzyunqi/common/third/weixin/AWeixinPayCbController.java
+++ src/main/java/cn/jzyunqi/common/third/weixin/AWeixinPayCbController.java
@@ -2,7 +2,7 @@ package cn.jzyunqi.common.third.weixin;
 
 import cn.jzyunqi.common.exception.BusinessException;
 import cn.jzyunqi.common.feature.pay.PayCallbackDto;
-import cn.jzyunqi.common.feature.pay.VerifyPayResult;
+import cn.jzyunqi.common.feature.pay.PayCallbackProcessor;
 import cn.jzyunqi.common.third.weixin.model.callback.PayResultCb;
 import jakarta.annotation.Resource;
 import org.springframework.http.HttpHeaders;
@@ -11,15 +11,13 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.math.BigDecimal;
-
 /**
  * 微信回调接口
  *
  * @author wiiyaya
  * @since 2021/5/9.
  */
-public abstract class AWeixinPayCbController {
+public abstract class AWeixinPayCbController implements PayCallbackProcessor {
 
     @Resource
     private WeixinPayStrange weixinPayStrange;
@@ -35,7 +33,7 @@ public abstract class AWeixinPayCbController {
         //组装回调对象
         PayCallbackDto payCallbackDto = new PayCallbackDto();
         payCallbackDto.setApplyPayNo(null); //支付申请单号
-        payCallbackDto.setActualPayType(null); //实际支付方式
+        payCallbackDto.setActualPayType("weixin"); //实际支付方式
         payCallbackDto.setActualPayNo(null); //实际支付单号
         payCallbackDto.setActualPayAmount(null); //实际支付金额
         payCallbackDto.setReturnHeaderMap(headers.toSingleValueMap());//支付接口返回header
@@ -43,27 +41,6 @@ public abstract class AWeixinPayCbController {
         payCallbackDto.setReturnParam(body); //支付接口返回的参数JSON字符串
         payCallbackDto.setReturnParamObject(payResultCb); //支付接口返回的参数对象
 
-        VerifyPayResult result = weixinPayStrange.verifyPayCallback(payCallbackDto);
-        switch (result) {
-            case SUCCESS:
-                this.paySuccess(payCallbackDto.getApplyPayNo(), payCallbackDto.getActualPayNo(), payCallbackDto.getActualPayAmount(), payCallbackDto.getReturnParam());
-                break;
-            case IGNORE:
-                break;
-            case FAILED:
-                throw new BusinessException("common_weixin_pay_call_back_failed", payCallbackDto.getActualPayType());
-            default:
-                break;
-        }
+        verifyPayCallback(weixinPayStrange, payCallbackDto);
     }
-
-    /**
-     * 支付成功回调
-     *
-     * @param applyPayNo      申请支付订单号
-     * @param actualPayNo     实际支付订单号
-     * @param actualPayAmount 实际支付金额
-     * @param returnParam     返回参数
-     */
-    protected abstract void paySuccess(String applyPayNo, String actualPayNo, BigDecimal actualPayAmount, String returnParam);
 }
