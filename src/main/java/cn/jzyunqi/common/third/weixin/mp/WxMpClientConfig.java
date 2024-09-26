6a24bdef16c61d076ca193b6c1772d5187547141
DarkDance <57419153@qq.com>
2024-09-26 17:40:51 +0800

    config修改为接口方式

--- src/main/java/cn/jzyunqi/common/third/weixin/mp/WxMpClientConfig.java
+++ src/main/java/cn/jzyunqi/common/third/weixin/mp/WxMpClientConfig.java
@@ -10,69 +10,25 @@ import java.util.Arrays;
  * @author wiiyaya
  * @since 2024/9/23
  */
-@Getter
-public class WxMpClientConfig {
+public interface WxMpClientConfig {
 
-    /**
-     * 公众号唯一凭证
-     */
-    private final String appId;
-    /**
-     * 公众号唯一凭证密钥
-     */
-    private final String appSecret;
+    String getAppId();
 
-    /**
-     * 消息tokrn
-     */
-    private final String msgToken;
+    String getAppSecret();
 
-    /**
-     * 用户信息同步中转页面
-     */
-    private final String userSyncUrl;
+    String getMsgToken();
 
-    /**
-     * 消息体加密密钥
-     */
-    private final byte[] msgEncodingAesKey;
+    String getUserSyncUrl();
 
-    /**
-     * 消息体加密向量
-     */
-    private final byte[] msgEncodingAesIv;
+    byte[] getMsgEncodingAesKey();
 
-    /**
-     * 客户端token key
-     */
-    private final String clientTokenKey;
+    byte[] getMsgEncodingAesIv();
 
-    /**
-     * js ticket key
-     */
-    private final String jsapiTicketKey;
+    String getJsapiTicketKey();
 
-    /**
-     * js ticket key
-     */
-    private final String sdkTicketKey;
+    String getWxCardTicketKey();
 
-    /**
-     * js ticket key
-     */
-    private final String wxCardTicketKey;
+    String getSdkTicketKey();
 
-    public WxMpClientConfig(String appId, String appSecret, String msgToken, String msgEncodingAesKey, String userSyncUrl) {
-        this.appId = appId;
-        this.appSecret = appSecret;
-        this.msgToken = msgToken;
-        this.msgEncodingAesKey = DigestUtilPlus.Base64.decodeBase64(msgEncodingAesKey);
-        this.userSyncUrl = userSyncUrl;
-
-        this.msgEncodingAesIv = Arrays.copyOfRange(Base64.decodeBase64(this.msgEncodingAesKey), 0, 16);
-        this.clientTokenKey = "client_token:" + appId;
-        this.jsapiTicketKey = "jsapi_ticket:" + appId;
-        this.sdkTicketKey = "sdk_ticket:" + appId;
-        this.wxCardTicketKey = "wx_card_ticket:" + appId;
-    }
+    String getClientTokenKey();
 }
