0dbbf7a5cfde5ba98bae2c06616acbdf624914b2
DarkDance <57419153@qq.com>
2024-09-26 15:49:37 +0800

    订阅消息处理

--- src/main/java/cn/jzyunqi/common/third/weixin/mp/template/model/WxMpTemplateMsgParam.java
+++ src/main/java/cn/jzyunqi/common/third/weixin/mp/template/model/WxMpTemplateMsgParam.java
@@ -27,9 +27,9 @@ public class WxMpTemplateMsgParam {
     private String templateId;
 
     /**
-     * 跳转网页时填写.
+     * 订阅通知消息专用
      */
-    private String page;
+    private Map<String, String> data;
 
     /**
      * 跳小程序所需数据，不需跳小程序可不用传该数据.
@@ -38,17 +38,33 @@ public class WxMpTemplateMsgParam {
     private MiniProgram miniProgram;
 
     /**
-     * 订阅通知消息专用
+     * 防重入id。对于同一个openid + client_msg_id, 只发送一条消息,10分钟有效,超过10分钟不保证效果。若无防重入需求，可不填
      */
-    private Map<String, String> data;
+    private String clientMsgId;
+
+    /**
+     * 模板消息专用：模板跳转链接（海外账号没有跳转能力）
+     */
+    private String url;
+
+    /**
+     * 订阅消息专用：跳转网页时填写.
+     */
+    private String page;
 
     @Getter
     @Setter
     public static class MiniProgram {
 
+        /**
+         * 所需跳转到的小程序appid（该小程序appid必须与发模板消息的公众号是绑定关联关系，暂不支持小游戏）
+         */
         @JsonProperty("appid")
         private String appId;
 
+        /**
+         * 所需跳转到小程序的具体页面路径，支持带参数,（示例index?foo=bar），要求该小程序已发布，暂不支持小游戏
+         */
         @JsonProperty("pagepath")
         private String pagePath;
     }
