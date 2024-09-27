9d7177e7db24ea3e790ecf19fb25ade2b02abbf4
DarkDance <57419153@qq.com>
2024-09-27 10:07:22 +0800

    配置修改为接口，方便后续通过其它方式实现

--- src/main/java/cn/jzyunqi/common/third/weixin/pay/WxPayClientConfig.java
+++ src/main/java/cn/jzyunqi/common/third/weixin/pay/WxPayClientConfig.java
@@ -3,46 +3,46 @@ package cn.jzyunqi.common.third.weixin.pay;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.sql.Blob;
+
 /**
  * @author wiiyaya
  * @since 2024/9/25
  */
-@Getter
-@AllArgsConstructor
-public class WxPayClientConfig {
+public interface WxPayClientConfig {
 
     /**
      * 应用唯一标识
      */
-    private final String appId;
+    String getAppId();
 
     /**
      * 支付回调URL
      */
-    private final String payCallbackUrl;
+    String getPayCallbackUrl();
 
     /**
      * 退款回调URL
      */
-    private final String refundCallbackUrl;
+    String getRefundCallbackUrl();
 
     /**
      * 支付商户号
      */
-    private final String merchantId;
+    String getMerchantId();
 
     /**
      * 支付商户证书私钥
      */
-    private final String merchantPrivateKey;
+    String getMerchantPrivateKey();
 
     /**
      * 支付商户证书序列号
      */
-    private final String merchantSerialNumber;
+    String getMerchantSerialNumber();
 
     /**
      * 支付商户AES key
      */
-    private final String merchantAesKey;
+    String getMerchantAesKey();
 }
