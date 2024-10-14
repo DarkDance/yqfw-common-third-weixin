f437568fd14770753b6e2852ffbdc257e9bc8fbc
DarkDance <57419153@qq.com>
2024-10-14 23:03:39 +0800

    类改动

--- src/main/java/cn/jzyunqi/common/third/weixin/open/WxOpenClientConfig.java
+++ src/main/java/cn/jzyunqi/common/third/weixin/open/WxOpenClientConfig.java
@@ -7,17 +7,15 @@ import lombok.Getter;
  * @author wiiyaya
  * @since 2024/9/25
  */
-@Getter
-@AllArgsConstructor
-public class WxOpenClientConfig {
+public interface WxOpenClientConfig {
 
     /**
      * 应用唯一标识
      */
-    private final String appId;
+    String getAppId();
 
     /**
      * 应用密钥
      */
-    private final String appSecret;
+    String getAppSecret();
 }
